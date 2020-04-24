39086b04c47c033aa70e1cbfa39fe490ad47d7e4
angelajanin <angelajanin@ciencias.unam.mx>
2020-04-24 12:33:00 -0500

    ejercicios completos tarea 2

--- Reposicion/repo.hs
+++ Reposicion/repo.hs
@@ -4,6 +4,7 @@ module Reposicion where
     import Data.Bool
     import Data.Char
 
+    -- Primer ejercicio Tarea 2
     todos :: Eq a => (a -> Bool) -> [a] -> Bool
     todos _ [] = True
     todos l m = if length (filter(l) m) == length m then True else False
@@ -18,11 +19,19 @@ module Reposicion where
     deja :: (a -> Bool) -> [a] -> [a]
     deja l = filter $ not . l
 
+    -- Segundo ejercicio Tarea 2
     altMap :: (a -> b)-> (a -> b)-> [a] -> [b]
     altMap m n [] = [] 
     altMap m n [o] = [m o]
     altMap m n [o, p] = [m o, n p] 
     altMap m n (x:y:ys) = m x : n y : altMap m n ys
+
+    --Tercer ejercicio Tarea 2
+    luhn :: [Int] -> Bool
+    luhn m = if (r `mod` 10) == 0 then True else False
+        where r = foldr (+) 0 [if (x > 9) then (x - 9) else x | x <- altMap (*2) (+0) m]
+
+
 
 
 
